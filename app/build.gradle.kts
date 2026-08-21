4b76b4601f65bfb3f60e307127a7bc078e06a7df
wizand0 <xabor@yandex.ru>
2026-08-21 14:14:00 +0300

    chore: bump version to 1.3.1, update app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ru.wizand.camwall"
         minSdk = 29
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
